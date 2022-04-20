a2c621aeba2786dad1846599dcb76ed85d1b375c
geraldwogan <geraldwogan@gmail.com>
2022-04-20 22:38:55 +0100

    Tidying GET data functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import json
 import requests
+import sys
 
 def data_cleaning(all_media):
     # Extract 'Movie' data from source dataset.
@@ -24,22 +25,28 @@ def get_secrets():
 
     return secrets
 
-def test_connection(api_key,  imdb_id):
+def get_data_from_api(api_key, imdb_id):
+    # Headers -> User-Agent
     needed_headers = {'User-Agent': "film-fact-finding/1.0"}
-    response = requests.get(f'https://api.themoviedb.org/3/find/{imdb_id}?api_key={api_key}&external_source=imdb_id', headers = needed_headers)
-    
-    print(response.status_code)
-    content = response.json()
-    # print(content['movie_results'][0]['popularity'])
 
+    # find/{imdb_id} endpoint
+    response = requests.get(f'https://api.themoviedb.org/3/find/{imdb_id}?api_key={api_key}&external_source=imdb_id', headers = needed_headers)
+
+    if response.status_code != '200':
+        sys.exit('Invalid API response {0}.'.format(response.status_code))
+
+    # Get movie info from return content
+    content = response.json()
     first_result = content['movie_results'][0]
 
     return first_result
+
 
 if __name__ == '__main__':
     src_data = pd.read_excel('data/2021 GW Media Tracking.xlsx', sheet_name='media_tracking', engine='openpyxl')
     films = data_cleaning(src_data)
     test_id = films.iloc[0]['imdb_id'] # tt10872600 - Spider-Man: No Way Home
+    
     secrets = get_secrets()
-    film =  test_connection(secrets['api_key'], test_id)
+    film =  get_data_from_api(secrets['api_key'], test_id)
     print(film)
